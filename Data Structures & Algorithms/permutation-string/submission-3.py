a0e2e5bfa61de9783cbb4eb79b5a3788e63554ef
saad <88393775+saad-7223@users.noreply.github.com>
2026-09-15 00:30:37 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        sd1 = {}
+        for ch in s1:
+            sd1[ch] = 1 + sd1.get(ch,0)
+        l = 0
+        r = len(s1)
+        while r <= len(s2):
+            sd2 = {}
+            window = s2[l:r]
+            if s2[l] in s1 and s2[r-1] in s1: 
+                for ch in window:
+                    sd2[ch] = 1 + sd2.get(ch,0)    
+                if sd1 == sd2:
+                    return True
+            l += 1
+            r += 1
+        return False
+            
